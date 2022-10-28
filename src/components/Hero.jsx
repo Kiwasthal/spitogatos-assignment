8dfa5650f384f754755ac5cbfc64c68a23b2ea53
Steerpike <imthekoul@gmail.com>
2022-10-28 20:31:34 +0300

    Hero layout

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <header className="hero">
+      <div className="hero-image"></div>
+      <div className="hero-modal">
+        <h1 className="hero-modal-title">S.und Co</h1>
+        <p className="hero-modal-content">
+          To take a trivial example, which of us ever undertakes laborious
+          physical exercise, except to obtain some advantage from it?
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
