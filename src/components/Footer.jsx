4cd2f9a306945eb3b90ce918cdbb39e7eefda04c
Steerpike <imthekoul@gmail.com>
2022-10-29 21:18:03 +0300

    Footer & styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import Logo from '../assets/Logo.svg';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <span className="footer-copywrite">© The standard Copywrite 2020</span>
+      <span className="footer-cookiesprivacy">Cookies. | Privacy.</span>
+      <img className="footer-logo" src={Logo} alt="" />
+    </footer>
+  );
+};
+
+export default Footer;
