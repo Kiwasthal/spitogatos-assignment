495322b9e75b5855e66b71cb30df075ebcfd7cba
Steerpike <imthekoul@gmail.com>
2022-10-29 21:17:39 +0300

    Navbar component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,53 @@
+import { useState } from 'react';
+import SlideShow from './SlideShow';
+
+/**
+ * Slider Array
+ */
+
+const slides = [
+  {
+    url: '/src/assets/Hero_Banner_One.png',
+  },
+  {
+    url: '/src/assets/Hero_Banner_Two.jpg',
+  },
+  {
+    url: '/src/assets/Hero_Banner_Three.jpg',
+  },
+];
+
 const Hero = () => {
+  /**
+   * Used to dictate the appearing slider
+   */
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
     <header className="hero">
-      <div className="hero-image"></div>
+      <SlideShow slides={slides} currentIndex={currentIndex} />
+      {/* //--- Text Modal --- // */}
       <div className="hero-modal">
         <h1 className="hero-modal-title">S.und Co</h1>
         <p className="hero-modal-content">
           To take a trivial example, which of us ever undertakes laborious
           physical exercise, except to obtain some advantage from it?
         </p>
+      </div>
+      {/* // --- Slider Buttons --- // */}
+      <div className="hero-slidenav">
+        {slides.map((_, index) => (
+          <div
+            key={index}
+            /** Only if the index of the button is the same as the displaying slide assign active class */
+            className={`${
+              currentIndex === index
+                ? 'hero-slidenav-item active'
+                : 'hero-slidenav-item '
+            } `}
+            onClick={() => setCurrentIndex(index)}
+          ></div>
+        ))}
       </div>
     </header>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,17 @@ const Nav = () => {
   return (
     <nav className="nav">
       <img src={Logo} alt="S.und Co Lime Logo" className="nav-logo" />
-      <ul className="menu-nav">
-        <li className="menu-nav-link active">Main One</li>
-        <li className="menu-nav-link">Page Two</li>
-        <li className="menu-nav-link">Page Three</li>
-        <li className="menu-nav-link">About Us</li>
-        <li className="menu-nav-link">Our Work</li>
-        <li className="menu-nav-link">Contact</li>
+      <ul className="nav-menu">
+        <li className="nav-menu-link active">Main One</li>
+        <li className="nav-menu-link">Page Two</li>
+        <li className="nav-menu-link">Page Three</li>
+        <li className="nav-menu-link">About Us</li>
+        <li className="nav-menu-link">Our Work</li>
+        <li className="nav-menu-link">Contact</li>
       </ul>
+      <h3 className="nav-localization">
+        <span>EN</span> | <span>GR</span>
+      </h3>
     </nav>
   );
 };
