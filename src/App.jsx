32b64b1fbfcff50b0591b4ad34e30e5ecf25a972
Steerpike <imthekoul@gmail.com>
2022-10-29 21:20:14 +0300

    Dependencies on main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './scss/main.scss';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import MainSection from './components/MainSection';
+import ContactSection from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <MainSection />
+      <ContactSection />
+      <Footer />
     </>
   );
 }
